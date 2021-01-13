59e330ed72583c71833cbaf023e406c49b087852
cansinator <jcansinoitk@gmail.com>
2021-01-13 13:05:04 -0600

    cambio de url

--- js/utilidades/constantes.js
+++ js/utilidades/constantes.js
@@ -1,2 +1,5 @@
-const CONSULTACATEGORIAS = 'http://localhost/paulescobarservicios/categorias/post.php';
-const CONSULTAIMAGENESXCATEGORIAS = 'http://localhost/paulescobarservicios/imagenescategorias/post.php';
+/* const CONSULTACATEGORIAS = 'http://localhost/paulescobarservicios/categorias/post.php';
+const CONSULTAIMAGENESXCATEGORIAS = 'http://localhost/paulescobarservicios/imagenescategorias/post.php'; */
+
+const CONSULTACATEGORIAS = 'https://marketingtux.com/paulescobarservicios/categorias/post.php';
+const CONSULTAIMAGENESXCATEGORIAS = 'https://marketingtux.com/paulescobarservicios/imagenescategorias/post.php';
